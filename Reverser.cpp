dc6159be6fb1d9a528d7be2c0c40de4e36ec2bec
javsav <javed.alam@student.adelaide.edu.au>
2024-08-07 23:14:44 +0930

    Modified the reverseDigit function's method of calculating the number of digits in the integer - now using repeated division by 10 rather than converting to a string for increased efficiency

--- Reverser.cpp
+++ Reverser.cpp
@@ -3,22 +3,35 @@
 
 std::string Reverser::reverseString(std::string characters) {
 
+  // Base case: one character remaining
   if (characters.size() == 1) {
     return characters;
   }
-  std::string temp = characters;
-  temp.pop_back();
-  return characters[characters.size() - 1] + reverseString(temp);
+
+  // Create shortened string with the final character erased
+  std::string shortened = characters;
+  shortened.pop_back();
+
+  // Concatenate the final character of the string with recursive calls on the shortened string
+  return characters[characters.size() - 1] + reverseString(shortened);
 }
 
 int Reverser::reverseDigit(int value) {
 
-  std::string digits = std::to_string(value);
-  int multiplier = digits.size() - 1;
+  // Calculate number of digits in current value
+  int numDigits = 0;
+  // Repeatedly divide by 10 until number == 0
+  do {
+    value /= 10;
+    numDigits++;
+  } while (value != 0);
 
+  // Base case: value has two digits.
   if (value/10 < 10) {
     return ((value % 10) * 10) + (value / 10);
   }
-  return ((value % 10) * pow(10,multiplier) + reverseDigit(value/10));
+  
+  // Modulo 10 returns the final digit, multiply by the number of digits and continue recursively
+  return ((value % 10) * pow(10, numDigits) + reverseDigit(value/10));
 }
 
