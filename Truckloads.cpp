70d4f8416e5daeaa5be46e4da58d9b9491ae1577
javsav <javed.alam@student.adelaide.edu.au>
2024-08-07 21:43:47 +0930

    Truckloads question

---
+++ Truckloads.cpp
@@ -0,0 +1,19 @@
+
+
+class Truckloads {
+
+  public:
+
+  int numTrucks(int numCrates, int loadSize) {
+
+    // Base case (return 1 for each individual pile when load size is reached)
+    if (numCrates <= loadSize) {
+      return 1;
+    }
+
+    // Recursively divide the load in half until the load size is reached
+    return numTrucks(numCrates/2, loadSize) + numTrucks(numCrates/2, loadSize);
+
+  }
+
+};
