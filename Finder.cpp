637f4940bcef065e6e2edf1ca7c9bebfca837b3e
javsav <javed.alam@student.adelaide.edu.au>
2024-08-22 12:35:08 +0930

    removed string_view

--- Finder.cpp
+++ Finder.cpp
@@ -9,7 +9,7 @@ std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
   size_t lastIndex = 0;
   
   for (size_t i = 1; i <= s2.size(); i++) {    
-    size_t found = s1.find(std::string_view(s2.substr(0, i)), lastIndex);
+    size_t found = s1.find(s2.substr(0, i), lastIndex);
     if (found != std::string::npos) {
       result[i - 1] = found;
       lastIndex = found;
