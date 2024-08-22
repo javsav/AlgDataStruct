8013644ee1315a2915fab08e815f0a6b3aa468f5
javsav <javed.alam@student.adelaide.edu.au>
2024-08-22 13:07:45 +0930

    prevent repeated checks of already matched characters

--- Finder.cpp
+++ Finder.cpp
@@ -8,18 +8,26 @@ std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
   
   size_t lastIndex = 0;
   
-  for (size_t i = 1; i <= s2.size(); i++) {    
-    size_t found = s1.find(s2.substr(0, i), lastIndex);
+  for (size_t i = 1; i <= s2.size(); i++) {
+    // Prevent repeated checks of already matched characters
+    if (s1[lastIndex + i - 1] == s2[i]) {
+      result[i - 1] = lastIndex;
+      continue;
+
+    } else {
+      size_t found = s1.find(s2.substr(0, i), lastIndex + 1);
+    
     if (found != std::string::npos) {
       result[i - 1] = found;
       lastIndex = found;
     } else {
       break;
     }
+
+    }
+        
+    
   }
   return result;
 }
 
-int main() {
-
-}
