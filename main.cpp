018b3d23bbfdf5bb9adb47006eab8e07e9eb2864
= <=>
2024-09-01 01:52:40 +0930

    university system is shit as fuck

--- main.cpp
+++ main.cpp
@@ -12,49 +12,58 @@ int main(int argc, char *argv[]) {
   QuickSort sort;
   // To store arguments for sorting and searching
   std::vector<int> list(argc - 1, 0);
-  // For conversion of chars to int
- 
-  // Iterate through argument vector
-  if (argc > 1) {
-    for (int i = 1; i < argc; i++) {
-      char* currentChar = argv[i];
-      int currentArg = 0;
-      // Iterate through current character array until null terminator
-      while (*currentChar != '\0') {
-        // Multiply by 10 for each digit after the first
-        currentArg *= 10;
-        // Handle negative sign
-        if (*currentChar == '-') {
-          currentChar++;
-          currentArg += (*currentChar - '0');
-          currentArg *= -1;
-          currentChar++;
-          continue;       
-        // Obtain the ASCII value by subtracting zero character and add to currentArg
-        } else {
-          currentArg += (*currentChar - '0');
-          currentChar++;
-          continue;
-        }        
+
+    // For conversion of chars to int
+    // UNIVERSITY SYSTEM SUCKS DOESN'T LET YOU WRITE YOUR OWN FUCKING CODE EVEN
+    // THOUGH YOU PUT HEAPS OF EFFORT INTO IT
+    // // Iterate through argument vector
+    // if (argc > 1) {
+    //   for (int i = 1; i < argc; i++) {
+    //     char* currentChar = argv[i];
+    //     int currentArg = 0;
+    //     // Iterate through current character array until null terminator
+    //     while (*currentChar != '\0') {
+    //       // Multiply by 10 for each digit after the first
+    //       currentArg *= 10;
+    //       // Handle negative sign
+    //       if (*currentChar == '-') {
+    //         currentChar++;
+    //         currentArg += (*currentChar - '0');
+    //         currentArg *= -1;
+    //         currentChar++;
+    //         continue;
+    //       // Obtain the ASCII value by subtracting zero character and add to
+    //       currentArg } else {
+    //         currentArg += (*currentChar - '0');
+    //         currentChar++;
+    //         continue;
+    //       }
+    //     }
+    //     // Add to list
+    //     list[i - 1] = currentArg;
+    //   }
+
+    if (argc > 1) {
+      for (int i = 1; i < argc; i++) {
+        std::string currentCharArray = argv[i];
+        int currentArg = stoi(currentCharArray);
+        list[i - 1] = currentArg;
       }   
-      // Add to list
-      list[i - 1] = currentArg;
+
+      // Sort list
+      sort.sort(list);
+
+      // Search for the number 1 in the list
+      if (search.search(list, 1)) {
+        std::cout << "true ";
+      } else {
+        std::cout << "false ";
+      }
+
+      // Print the sorted list
+      for (int number : list) {
+        std::cout << number << " ";
+      } std::cout << "\n";
     }
-    // Sort list
-    sort.sort(list);
-
-    // Search for the number 1 in the list
-    if (search.search(list, 1)) {
-      std::cout << "true ";
-    } else {
-      std::cout << "false ";
-    }
-
-    // Print the sorted list
-    for (int number : list) {
-      std::cout << number << " ";
-    } std::cout << "\n";
-
-  }
-  return 0;  
+  return 0;
 }
