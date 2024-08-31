8798913df84afb693f5a02a4bb57546ed2414425
javsav <javed.alam@student.adelaide.edu.au>
2024-09-01 03:06:31 +0930

    test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-  std::cout << "TEST:" << argv[0] << "\n";
+  std::cout << "TEST:" << argv[0] << argv[1] << "\n";
   //std::cout << argv[0] << "\n";
   //std::cout << argv[1] << "\n";
   RecursiveBinarySearch search;
