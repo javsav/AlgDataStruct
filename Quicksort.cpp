7604df37f30e012b9f9756c615e47563e1f20469
= <=>
2024-08-31 22:23:53 +0930

    Quicksort iterative completed

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -16,35 +16,6 @@ std::vector<int>& Quicksort::sort(std::vector<int>& list) {
    
 }
 
-std::vector<int>& Quicksort::sortIterative(std::vector<int>& list) {
-
-  struct quickSortCall {
-    std::vector<int>& list;
-    int left;
-    int right;
-  };
-
-  std::stack<quickSortCall> callStack;
-
-  quickSortCall initialCall{list, 0, list.size() - 1};
-  
-  while ((callStack.top().right - callStack.top().left) > 0) {
-    std::vector<int>& listRef = callStack.top().list;
-    int& left = callStack.top().left;
-    int& right = callStack.top().right;
-    size_t size = callStack.top().right - callStack.top().left + 1;
-    size_t pivotIndex = callStack.top().left;
-
-    int pivotValue = listRef[right];
-    
-  }
-
-
-
-
-}
-
-
 void Quicksort::quickSortHelper(std::vector<int>& list, int left, int right) {
 
   // Base Case: return if the list has 1 or less items
@@ -59,13 +30,13 @@ void Quicksort::quickSortHelper(std::vector<int>& list, int left, int right) {
   // Set pivotIndex to start of current list section  
   size_t pivotIndex = left;
   
-
-  // Choose third item as pivot if size > 3 and swap with end of list section
+  // Choose third item as pivot if size > 3 and swap with end of list section (assignment requirement
   if (size > 3) {
     std::swap(list[left + 2], list[right]);       
-  }
+  } // Otherwise use the final element
 
-  // Pivot value is at the end of the section
+
+  // Pivot value is at the final index of current list
   int pivotValue = list[right];
 
 
@@ -78,22 +49,89 @@ void Quicksort::quickSortHelper(std::vector<int>& list, int left, int right) {
 
   // Place the pivot in its appropriate index
   std::swap(list[pivotIndex], list[right]);
-
+  
   // Recursively call the algorithm on the sections below and above the pivot
   quickSortHelper(list, left, pivotIndex - 1);
   quickSortHelper(list, pivotIndex + 1, right); 
-
+  
   
 }
+
+std::vector<int>& Quicksort::sortIterative(std::vector<int>& list) {
+
+  // Struct to be stored within the virtual stack calls
+  struct quickSortCall {
+    // List is not required as we have a pointer to it already and each call works on the same list
+    int left;
+    int right;
+  };
+  
+  // Use std::stack to emulate the call stack 
+  std::stack<quickSortCall> callStack;
+
+ 
+  // Push the first 'call' to the virtual stack so that the loop has something to process
+  callStack.push(quickSortCall{0, (int)list.size() - 1});
+
+  // Continue processing stack 'frames' until the virtual stack is empty
+  while (!callStack.empty()) {
+    // Copy the current virtual stack call left and right pointers
+    int left = callStack.top().left;
+    int right = callStack.top().right;
+    
+    // Define current list size and pivotIndex
+    size_t size = left - right + 1;
+    int pivotIndex = left;
+
+    // If 'base case' is reached, pop the current stack 'frame' and continue to next iteration
+    if (left >= right) {
+      callStack.pop();
+      continue;
+    }
+
+    // Choose third item as pivot if size > 3 and swap with end of list section (assignment requirement)
+    if (size > 3) {
+      std::swap(list[left + 2], list[right]);       
+    } // Otherwise use the final element
+
+    // Pivot value is at the final index of the current list
+    int pivotValue = list[right];
+
+    for (size_t i = left; i < right; i++) {
+      // Swap smaller/equal items with the pivot index and increment the index
+      if (list[i] <= pivotValue) {
+        std::swap(list[i], list[pivotIndex++]);
+      }
+    }
+
+    // Place the pivot in its appropriate index
+    std::swap(list[pivotIndex], list[right]);
+
+    // Remove the current "recursive call" from the stack
+    callStack.pop();
+    
+    // Push the next "recursive call structs" to the stack    
+    callStack.push(quickSortCall{left, pivotIndex - 1});   
+    callStack.push(quickSortCall{pivotIndex + 1, right});   
+  
+  }
+
+  return list;
+
+}
+
+
+
 
 // int main() {
 //   Quicksort sorter;
 
 //    std::vector<int> list = {2,7,4,1,3,2,6,7,99,5,4,234,3};
 
-//     std::vector<int> newlist = sorter.sort(list);
+//   sorter.sortIterative(list);
 
-//   for (auto i = newlist.begin(); i != newlist.end(); ++i) {
-//     std::cout << *i << ", ";
+//   for (auto i = list.begin(); i != list.end(); ++i) {
+//       std::cout << *i << ", ";
 //   }
+  
 // }
