46ea56412e1fdb1147203cb881c1ab3d43667f90
javsav <javed.alam@student.adelaide.edu.au>
2024-10-03 19:45:41 +0930

    more advanced template specialisation to reduce array size for integer trie

--- TrieNode.cpp
+++ TrieNode.cpp
@@ -1,24 +1,21 @@
 #include "TrieNode.h"
 
-template<>
 TrieNode<char>::TrieNode(char letter, bool isLeaf) : data(letter), isLeaf(isLeaf) {}
 
-template<>
 TrieNode<int>::TrieNode(int letter, bool isLeaf) : data(letter), isLeaf(isLeaf) {}
 
-template<>
 TrieNode<char>::TrieNode(char letter) : TrieNode::TrieNode(letter, false) {}
 
-template<>
 TrieNode<int>::TrieNode(int letter) : TrieNode::TrieNode(letter, false) {}
 
-template<>
 TrieNode<char>::TrieNode() : TrieNode::TrieNode('.', false) {}
 
-template<>
 TrieNode<int>::TrieNode() : TrieNode::TrieNode('.', false) {}
 
-template<>
-TrieNode<int>::operator char() {
+TrieNode<char>::operator char() {
+  return this->data;
+}
+
+TrieNode<int>::operator int() {
   return this->data;
 }
--- TrieNode.h
+++ TrieNode.h
@@ -2,18 +2,41 @@
 #define TRIENODE_H
 
 #define chars 26
+#define digits 10
 
 template<class T>
-class TrieNode {
- public:
-  T data;
-  TrieNode<T>* children[chars] {{}};
-  int isLeaf = false;  
-  //friend class Trie;
+class TrieNode;
+
+template<>
+class TrieNode<char> {
+
+ private:
+  char data;
+  TrieNode<char>* children[chars] {{}};
+  bool isLeaf = false; 
+  template<class T> friend class Trie;
+  
  public:
   operator char();
-  TrieNode(T letter);
-  TrieNode(T letter, bool isLeaf);
+  TrieNode(char letter);
+  TrieNode(char letter, bool isLeaf);
+  TrieNode();
+  
+};
+
+template <>
+class TrieNode<int> {
+
+ private:
+  int data;
+  TrieNode<int>* children[digits]{{}};
+  int isLeaf = false;
+  template<class T> friend class Trie;
+
+ public:
+  operator int();
+  TrieNode(int letter);
+  TrieNode(int letter, bool isLeaf);
   TrieNode();
 };
 
