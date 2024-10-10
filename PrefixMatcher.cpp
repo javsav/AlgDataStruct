a30a2f8f0a8112832bb8d80c9d9d34d5632f4605
javsav <javed.alam@student.adelaide.edu.au>
2024-10-10 12:04:54 +1030

    starting practical

--- PrefixMatcher.cpp
+++
@@ -1,9 +0,0 @@
-#include "PrefixMatcher.h"
-
-int PrefixMatcher::selectRouter(std::string networkAddress) {
-  return RouterDatabase.findRouter(networkAddress);
-}
-
-void PrefixMatcher::insert(std::string address, int routerNumber) {
-  RouterDatabase.insertRouter(address, routerNumber);
-}
