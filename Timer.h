886cf219c7cf6eb6e5f27022bbe33d4be60465de
= <=>
2024-11-09 23:10:30 +1030

    sorting algorithms

---
+++ Timer.h
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <iostream>
+#include <vector>
+class Timer {
+ private:
+  std::chrono::_V2::high_resolution_clock::time_point m_startTime;
+  std::chrono::_V2::high_resolution_clock::time_point m_endTime;
+  std::chrono::milliseconds m_elapsedTime;
+
+ public:
+  void startTimer();
+  void stopTimer();
+  std::chrono::milliseconds time();
+};
+
